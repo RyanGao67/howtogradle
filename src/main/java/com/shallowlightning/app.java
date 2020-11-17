3616d4bc2efb84e25276668d896948b85731f2be
RyanGao67 <tiangao0611@gmail.com>
2020-11-16 23:19:11 -0500

    a

---
+++ src/main/java/com/shallowlightning/app.java
@@ -0,0 +1,10 @@
+package com.shallowlightning;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class app {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println(StringUtils.isBlank(" "));
+    }
+}
